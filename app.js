e9d6605b8208a80ea9c54e28a90a761bb1727eeb
Federico Vidueiro <vidueiro@eudaimonia.com.ar>
2021-04-27 13:31:18 -0300

    MP checkout pro impl

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ var exphbs  = require('express-handlebars');
 var port = process.env.PORT || 3000
 
 var app = express();
- 
+var bodyParser = require('body-parser')
+
+var mp = require('./mp');
+
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.json()) // for parsing application/json
 
 app.use(express.static('assets'));
  
@@ -17,6 +23,28 @@ app.get('/', function (req, res) {
 
 app.get('/detail', function (req, res) {
     res.render('detail', req.query);
+});
+
+app.post('/pay', async function (req, res) {
+    let checkoutUrl = await mp.checkoutUrl(req.body, req.get('origin'));
+    res.redirect(checkoutUrl);
+});
+
+app.get('/success', function (req, res) {
+    res.render('success', req.query);
+});
+
+app.get('/pending', function (req, res) {
+    res.render('pending', req.query);
+});
+
+app.get('/failure', function (req, res) {
+    res.render('failure', req.query);
+});
+
+app.post('/notifications', function (req, res) {
+    console.log(JSON.stringify(req.body))
+    res.sendStatus(200);
 });
 
 app.listen(port);
---
+++ mp.js
@@ -0,0 +1,57 @@
+// SDK de Mercado Pago
+const mercadopago = require('mercadopago');
+// Agrega credenciales
+mercadopago.configure({
+    access_token: process.env.MP_ACCESS_TOKEN,
+    integrator_id: process.env.MP_INTEGRATOR_ID
+});
+
+module.exports = {
+
+    checkoutUrl: async (item, host) => {
+        // Crea un objeto de preferencia
+        let preference = {
+            items: [
+                {
+                    id: 1234,
+                    title: item.title,
+                    description: "Dispositivo móvil de Tienda e-commerce",
+                    picture_url: host + '/' + item.img,
+                    unit_price: parseInt(item.price),
+                    quantity: parseInt(item.unit),
+                }
+            ],
+            external_reference: process.env.EMAIL,
+            payment_methods: {
+                excluded_payment_methods: [{id:'amex'}],
+                excluded_payment_types: [{payment_type_id:'atm'}],
+                installments: 6
+            },
+            payer: {
+                name: process.env.PAYER_NAME,
+                surname: process.env.PAYER_SURNAME,
+                email: process.env.PAYER_EMAIL,
+                phone: {
+                    area_code: process.env.PAYER_AREA_CODE,
+                    number: parseInt(process.env.PAYER_PHONE)
+                },
+                address: {
+                    zip_code: process.env.PAYER_ZIPCODE,
+                    street_name: process.env.PAYER_ADDRESS_STREET,
+                    street_number: parseInt(process.env.PAYER_ADDRESS_STREET_NUMBER)
+                }
+            },
+            auto_return: "all",
+            back_urls: {
+                success: host + "/success",
+                pending: host + "/pending",
+                failure: host + "/failure"
+            },
+            notification_url: host + "/notifications"
+        };
+
+        var response = await mercadopago.preferences.create(preference)
+        return response.body.init_point
+    }
+
+};
